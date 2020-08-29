d1d55ecb778ab6d9bdb1695c65009a3f34e182ad
Vladimir Lebedev <zeronineseven@gmail.com>
2020-08-30 00:22:34 +0300

    auto deployer for Glitch implementation attempt;

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 const express = require("express");
 const bodyParser = require("body-parser");
+const cmd = require("node-cmd");
+const crypto = require("crypto");
 const app = express();
 const fs = require("fs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -99,6 +101,35 @@ app.get("/clearDreams", (request, response) => {
         );
     }
 });
+
+// Github webhook listener
+app.post('/git', verifySignature, (req, res) => {
+  if (req.headers['x-github-event'] == 'push') {
+    cmd.get('bash git.sh', (err, data) => {
+      if (err) return console.log(err)
+      console.log(data)
+      cmd.run('refresh')
+      return res.status(200).send(data)
+    })
+  } else if(req.headers['x-github-event'] == 'ping') {
+    return res.status(200).send('PONG')
+  } else {
+    return res.status(200).send('Unsuported Github event. Nothing done.')
+  }
+})
+
+const verifySignature = (req, res, next) => {
+  const payload = JSON.stringify(req.body)
+  const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET)
+  const digest = 'sha1=' + hmac.update(payload).digest('hex')
+  const checksum = req.headers['x-hub-signature']
+
+  if (!checksum || !digest || checksum !== digest) {
+    return res.status(403).send('auth failed')
+  }
+
+  return next()
+};
 
 // helper function that prevents html/css/script malice
 const cleanseString = function(string) {
